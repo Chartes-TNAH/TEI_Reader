f285b9b42c76d02650dae90f1e0681f80fc4f7fc
Alexandre Bartz <alex.bartz@outlook.fr>
2020-03-22 00:03:02 +0100

    Début de css

--- TEI_Reader/app.py
+++ TEI_Reader/app.py
@@ -4,10 +4,12 @@ import os
 
 chemin_actuel = os.path.dirname(os.path.abspath(__file__))
 templates = os.path.join(chemin_actuel, "templates")
+statics = os.path.join(chemin_actuel, "static")
 
 app = Flask(
     "Application",
-    template_folder=templates
+    template_folder=templates,
+    static_folder=statics
 )
 
 import TEI_Reader.routes
